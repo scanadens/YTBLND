ea05c7662da7b35fce7f0e4046bc8eba0cb37533
Jasmine_Kumar <Jkumar43@uwo.ca>
2026-03-12 16:00:53 -0400

    chore: add root .gitignore to exclude build/ artifacts

--- build/YTBLND_src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/slipnut44/git_repo/group45/YTBLND_src
-# Build directory: /home/slipnut44/git_repo/group45/build/YTBLND_src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
