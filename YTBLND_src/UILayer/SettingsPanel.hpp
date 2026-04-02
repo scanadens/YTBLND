abef877140f88b681ea67c3336da8d6cddb91ba2
Xavier Wah-Huen Lusetti <xlusetti@uwo.ca>
2026-04-02 08:40:34 +0000

    Replace SettingsPanel.hpp

--- YTBLND_src/UILayer/SettingsPanel.hpp
+++ YTBLND_src/UILayer/SettingsPanel.hpp
@@ -13,16 +13,16 @@ class wxChoice;
 
 class SettingsPanel : public wxPanel {
 public:
-	SettingsPanel(wxWindow* parent, AppController& controller, NavigateFn nav);
+    SettingsPanel(wxWindow* parent, AppController& controller, NavigateFn nav);
 
 private:
-	AppController& m_controller;
-	NavigateFn     m_nav;
-	wxChoice*      m_themeChoice = nullptr;
+    AppController& m_controller;
+    NavigateFn     m_nav;
+    wxChoice* m_themeChoice = nullptr;
 
-	void RefreshUserInfo();
+    void RefreshUserInfo();
 
-	void OnShow(wxShowEvent& evt);
-	void OnThemeChanged(wxCommandEvent& evt);
-	void OnLogout(wxCommandEvent& evt);
+    void OnShow(wxShowEvent& evt);
+    void OnThemeChanged(wxCommandEvent& evt);
+    void OnLogout(wxCommandEvent& evt);
 };
